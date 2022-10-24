65582d7f9026ccfcd714ddf60c917ccf9ce12eac
bartosz-laszczuk <bartosz.laszczuk@outlook.com>
2022-10-24 17:12:15 +0200

    Solution initialization

---
+++ apps/question-randomizer/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-projects-nx-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'question-randomizer';
+}

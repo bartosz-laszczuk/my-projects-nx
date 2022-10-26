2dad6d574550037bf9378e2e2ed838aceaa2fb8d
bartosz-laszczuk <bartosz.laszczuk@outlook.com>
2022-10-26 15:18:14 +0200

    Crt theme library implemented

--- libs/shared/layout/ui-crt/src/lib/shared-layout-ui-crt/shared-layout-ui-crt.component.ts
+++ libs/shared/layout/ui-crt/src/lib/shared-layout-ui-crt/shared-layout-ui-crt.component.ts
@@ -1,17 +1,10 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  ViewEncapsulation,
-} from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'my-projects-nx-shared-layout-ui-crt',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './shared-layout-ui-crt.component.html',
   styleUrls: ['./shared-layout-ui-crt.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  // encapsulation: ViewEncapsulation.None,
 })
 export class SharedLayoutUiCrtComponent {}

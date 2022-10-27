7e9ef15eb819101e232e2f0463db193dcdf01129
bartosz-laszczuk <bartosz.laszczuk@outlook.com>
2022-10-27 08:40:26 +0200

    Added Angular Material to solution

--- apps/question-randomizer/src/app/app.module.ts
+++ apps/question-randomizer/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { SharedLayoutUiCrtComponent } from '@my-projects-nx/shared/layout/ui-crt';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedLayoutUiCrtComponent],
+  imports: [BrowserModule, SharedLayoutUiCrtComponent, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })

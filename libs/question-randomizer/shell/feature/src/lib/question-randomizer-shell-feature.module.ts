59285ba189d109abd419640d2714cafec70fb637
bartosz-laszczuk <bartosz.laszczuk@outlook.com>
2022-11-03 15:00:22 +0100

    Added NgRx libraries

--- libs/question-randomizer/shell/feature/src/lib/question-randomizer-shell-feature.module.ts
+++ libs/question-randomizer/shell/feature/src/lib/question-randomizer-shell-feature.module.ts
@@ -3,12 +3,35 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { questionRandomizerFeatureShellRoutes } from './lib.routes';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(questionRandomizerFeatureShellRoutes),
+    StoreModule.forRoot(
+      {
+        // common: commonReducer,
+        // dictionaries: dictionariesReducer,
+        // user: userReducer,
+      },
+      {
+        runtimeChecks: {
+          strictStateImmutability: true,
+          strictActionImmutability: true,
+        },
+      }
+    ),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      // logOnly: environment.production,
+    }),
+    // EffectsModule.forRoot([CommonEffects, DictionariesEffects, UserEffects]),
+    StoreRouterConnectingModule.forRoot(),
   ],
   exports: [RouterModule],
 })
